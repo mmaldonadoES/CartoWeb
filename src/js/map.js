276e3b7e8a219d7210082c53139e35ca41a7eab3
120m4n <romansarmientogomez@gmail.com>
2021-12-14 15:40:59 -0500

    Agregar tooltip con atributos de la capa

--- src/js/map.js
+++ src/js/map.js
@@ -2,14 +2,25 @@ const attribution = new ol.control.Attribution({
     collapsible: false,
 });
 
-var map = new ol.Map({
+let MapaBase = new ol.layer.Tile({
+    source: new ol.source.OSM()
+})
+
+let map = new ol.Map({
     target: 'map',
     controls: ol.control.defaults({
         attribution: false
-    }).extend([attribution])
+    }).extend([attribution]),
+    layers: [MapaBase, vector_Apoyos],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([-73.13, 7.13]),
+        minZoom: 1,
+        maxZoom: 20,
+        zoom: 11
+    })
 });
 
-var sidebar = new ol.control.Sidebar({
+let sidebar = new ol.control.Sidebar({
     element: 'sidebar',
     position: 'left'
 });
@@ -18,17 +29,17 @@ map.addControl(sidebar);
     
 // Button Control
 // Add a custom push button with onToggle function
-var hello = new ol.control.Button({
+let hello = new ol.control.Button({
     html: '<i class="fa fa-smile-o"></i>',
     className: "hello",
     title: "Hello world!",
     handleClick: function () {
-        info("hello World!");
+        showinfo("hello World!");
     }
 });
 map.addControl(hello);
 
 // Show info
-function info(i) {
+function showinfo(i) {
     console.log(i);
 }
