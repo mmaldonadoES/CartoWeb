9804085f1891bf17d2ee9eff0e560fa64f9b9b53
fgafaro <fgafaro@electrosoftware.net>
2021-12-17 08:31:06 -0500

    Agregué la funcionalidad en un archivo js  MousePotsition.js

---
+++ src/js/MousePosition.js
@@ -0,0 +1,10 @@
+// Posición del Mouse 
+const mousePositionControl = new ol.control.MousePosition({
+    coordinateFormat: ol.coordinate.createStringXY(8),
+    projection: 'EPSG:4326',
+    // comment the following two lines to have the mouse position
+    // be placed within the map.
+    className: 'custom-mouse-position',
+    target: document.getElementById('mouse-position'),
+  });
+  map.addControl(mousePositionControl);
