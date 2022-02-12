343640ca510a62ee085928d4ec6c515d7bda8283
jorgeruedaes@gmail.com <jorgeruedaes>
2022-02-12 07:36:54 -0500

    Agregar capa base

--- src/js/utilidadesMapa.js
+++ src/js/utilidadesMapa.js
@@ -1,3 +1,5 @@
+$modal_mapa_base = document.getElementById('modal_mapa');
+
 function BuscarApoyoCartografia() {
   $modal_mapa = document.getElementById('buscar_apoyo');
 
@@ -91,3 +93,95 @@ function UbicarEnMapaXY(x, y) {
   pulse(ol.proj.transform([x_c,y_c], MagnaSirgas, 'EPSG:4326'))
 
 }
+
+
+
+$modal_mapa_base.addEventListener('click', async () => {
+
+
+   const { value: formValues } = await Swal.fire({
+      title: 'Agregar nueva Capa',
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      showCancelButton: true,
+      cancelButtonText: 'Cerrar',
+      html:
+         ' <div class="mb-3">' +
+         '<Label>Nombre</label>' +
+         '<input id="swal-input1" class="swal2-input">' +
+         '</div>' +
+         ' <div class="mb-3">' +
+         '<Label>Ruta</label>' +
+         '<input id="swal-input2" class="swal2-input">' +
+         '</div>' +
+         ' <div class="mb-3">' +
+         '<Label>Z Mínimo</label>' +
+         '<input type="number" id="swal-input3" class="swal2-input">' +
+         '</div>' +
+         ' <div class="mb-3">' +
+         '<Label>Z Máximo</label>' +
+         '<input type="number" id="swal-input4" class="swal2-input">' +
+         '</div>',
+      focusConfirm: true,
+      preConfirm: () => {
+         let json = {
+            "code":  Math.floor(Math.random()*394)+10,
+            "name": document.getElementById('swal-input1').value,
+            "route": document.getElementById('swal-input2').value,
+            "minZoom": document.getElementById('swal-input3').value,
+            "maxZoom": document.getElementById('swal-input4').value,
+         };
+         if (isValidURL(json.route)){
+         if (parseInt(json.minZoom) < parseInt(json.maxZoom)) {
+            if (parseInt(json.maxZoom) >= 0 && parseInt(json.maxZoom) <= 20) {
+               if (parseInt(json.minZoom) >= 0 && parseInt(json.minZoom) <= 20) {
+                  if (validarModal(json)) {
+                     datos.push(json);
+                     document.querySelector("#selecetMapaBase").innerHTML = '';
+                     userAction();
+                     Swal.fire(
+                        'Información',
+                        'El mapa base se ha agregado con éxito',
+                        'information'
+                     );
+
+                  } else {
+                     Swal.showValidationMessage('Ha ocurrido un error, todos los datos son obligatorios.');
+                  }
+               } else {
+                  Swal.showValidationMessage('Ha ocurrido un error, El nivel de Zoom mínimo debe estar entre 0 y 20');
+               }
+            } else {
+               Swal.showValidationMessage('Ha ocurrido un error, El nivel de Zoom máximo debe estar entre 0 y 20');
+            }
+         } else {
+            Swal.showValidationMessage('Ha ocurrido un error, El nivel de Zoom mínimo debe ser menor al Zoom máximo');
+         }
+      } else {
+         Swal.showValidationMessage('Ha ocurrido un error, La ruta ingresada no es valida, ingrese otra e intente nuevamente');
+      }
+         return json;
+      }
+   });
+
+});
+
+
+
+function validarModal  (formValues) {
+if (formValues.name != '') {
+   if (formValues.route != '') {
+      if (formValues.minZoom != '') {
+         if (formValues.maxZoom != '') {
+            return true;
+         } else { return false }
+      } else { return false }
+   } else { return false }
+} else { return false };
+};
+
+
+function isValidURL (string) {
+var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+return (res !== null)
+};
